e7f1b9e158ea22bbe4e7fd97d51756b3936ecc75
theSwapnilZambare <swapnil.zambare2@gmail.com>
2020-05-06 22:27:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/search/AdapterSearch.java
@@ -0,0 +1,87 @@
+package com.example.search;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AdapterSearch extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private LayoutInflater inflater;
+    List<DataSearch> data= Collections.emptyList();
+    DataSearch current;
+
+    // create constructor to initialize context and data sent from MainActivity
+    public AdapterSearch(Context context, List<DataSearch> data){
+        this.context=context;
+        inflater= LayoutInflater.from(context);
+        this.data=data;
+    }
+
+    // Inflate the layout when ViewHolder created
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.container_search, parent,false);
+        MyHolder holder=new MyHolder(view);
+        return holder;
+    }
+
+    // Bind data
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        // Get current position of item in RecyclerView to bind data and assign values from list
+        MyHolder myHolder= (MyHolder) holder;
+        DataSearch current=data.get(position);
+        myHolder.textFishName.setText(current.productName);
+        myHolder.textSize.setText("Company: " + current.productCompany);
+        myHolder.textType.setText("Category: " + current.category);
+        myHolder.textPrice.setText("Rs. " + current.productPrice + "\\Kg");
+        myHolder.textPrice.setTextColor(ContextCompat.getColor(context, R.color.colorAccent));
+
+    }
+
+    // return total item from List
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    class MyHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView textFishName;
+        TextView textSize;
+        TextView textType;
+        TextView textPrice;
+
+        // create constructor to get widget reference
+        public MyHolder(View itemView) {
+            super(itemView);
+            textFishName= (TextView) itemView.findViewById(R.id.textFishName);
+            textSize = (TextView) itemView.findViewById(R.id.textSize);
+            textType = (TextView) itemView.findViewById(R.id.textType);
+            textPrice = (TextView) itemView.findViewById(R.id.textPrice);
+            itemView.setOnClickListener(this);
+        }
+
+        // Click event for all items
+        @Override
+        public void onClick(View v) {
+
+            Toast.makeText(context, "You clicked an item", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/search/DataSearch.java
@@ -0,0 +1,8 @@
+package com.example.search;
+
+public class DataSearch {
+        public String productName;
+        public String category;
+        public String productCompany;
+        public int productPrice;
+    }
